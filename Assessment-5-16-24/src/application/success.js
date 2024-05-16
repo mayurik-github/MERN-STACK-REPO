85b1c550f2ad1f1cc05baab781703c3a30844076
mayurik-github <161001486+mayurik-github@users.noreply.github.com>
2024-05-16 10:03:46 -0700

    code for 1 & 3

---
+++ Assessment-5-16-24/src/application/success.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+export default class Success extends Component {
+    render() {
+        return(
+            <div>
+                <h4>This is Success Component from React App.</h4> 
+            </div>
+            
+        )
+    }
+}
---
+++ Assessment-5-16-24/src/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import * as ReactDOM from "react-dom/client";
+import Success from "./application/success";
+
+console.log("In index.js file")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <Success/>
+)
